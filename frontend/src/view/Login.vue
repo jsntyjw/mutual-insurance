d86f2e65e1fcfec80d35ce593fb7c3cee2f146b4
jackie1950057 <2821166215@qq.com>
2024-04-27 16:31:36 +0800

    update

--- frontend/src/view/Login.vue
+++ frontend/src/view/Login.vue
@@ -17,30 +17,42 @@
     </svg>
   </div>
   <div class="flex w-full h-full">
-    <div class="justify-center hidden md:flex flex-1 md:pt-20">
+    <div class="justify-center hidden md:flex flex-1 md:pt-12">
       <div class="translate-x-3 text-primary-content ">
         <h1 class=" font-nun font-extrabold normal-case text-6xl">
           Mutual Insurance
         </h1>
         <h2 class="font-title text-lg font-extrabold sm:text-xl lg:text-2xl">
-          Your best career safe guard
+          Secure and Transparent Mutual Insurance on Blockchain
         </h2>
 
-        <div class="mt-12 flex max-w-sm flex-col gap-2 text-left">
-          <div class="flex gap-1">
-            great helper for managing photos and albums
-          </div>
-          <div class="flex gap-1">
-            A place to share moments with your kindred spirits
-          </div>
-          <div class="flex gap-1">
-            Faster and safer
-          </div>
+
+        <div class="mt-12 flex flex-col text-left">
+          <div class="font-bold text-lg">Your Monthly Premium Contributions</div>
+          <ul class="list-disc pl-5">
+            <li>Tier 1 (Monthly Salary up to $2,000): $20 per month</li>
+            <li>Tier 2 (Monthly Salary $2,001 - $5,000): $40 per month</li>
+            <li>Tier 3 (Monthly Salary $5,001 - $10,000): $60 per month</li>
+            <li>Tier 4 (Monthly Salary above $10,000): $80 per month</li>
+          </ul>
         </div>
+
+        <div class="mt-12 flex flex-col text-left">
+          <div class="font-bold text-lg">Payout Plan</div>
+          <ul class="list-disc pl-5">
+            <li>Tier 1: 50% of the participant's average monthly salary, capped at $1,000 per month</li>
+            <li>Tier 2: 40% of the participant's average monthly salary, capped at $2,000 per month</li>
+            <li>Tier 3: 30% of the participant's average monthly salary, capped at $3,000 per month</li>
+            <li>Tier 4: 20% of the participant's average monthly salary, capped at $4,000 per month</li>
+          </ul>
+        </div>
+
+
+
       </div>
 
     </div>
-    <div class="flex flex-1 items-start justify-center pt-5 md:pt-20 mb-6">
+    <div class="flex flex-1 items-start justify-center pt-5 md:pt-12 mb-6">
       <div class="md:-translate-x-4 card w-11/12 max-w-sm md:max-w-xs lg:max-w-md glass p-5 pb-7">
         <div class="tabs">
           <a
