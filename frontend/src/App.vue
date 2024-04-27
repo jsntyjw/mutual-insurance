b11df1def7dd068f6d0ed642c2ac76b020a2ed23
jackie1950057 <2821166215@qq.com>
2024-04-27 15:17:33 +0800

    update

---
+++ frontend/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Main from './view/Main.vue'
+import ChangeTheme from './components/ChangeTheme.vue'
+function handleWalletConnection() {
+  console.log('小志1')
+
+}
+</script>
+
+<template>
+<!--  <HelloWorld msg="Vite + Vue" />-->
+  <div>
+    <div class="bg-base-300 h-12 flex justify-between items-center px-5">
+      <div>
+        Account: 0xjfsdklakhfdgjfls
+      </div>
+      <div>
+        <div @click="handleWalletConnection" class="btn btn-sm btn-ghost">Connect Wallet</div>
+
+        <change-theme></change-theme>
+      </div>
+
+    </div>
+    <Main></Main>
+  </div>
+</template>
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ frontend/src/view/HR.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "HR"
+}
+</script>
+
+<style scoped>
+
+</style>
