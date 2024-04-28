d54b869cc5369f56b4b5872e951aea8fde923b0b
Zhuohua Zhong <zhong_zhuohua@outlook.com>
2024-04-29 00:35:39 +0800

    update abi and contract address

--- frontend/src/contract/contractConstants.js
+++ frontend/src/contract/contractConstants.js
@@ -1,4 +1,4 @@
-export const contractAddress = '0x956B781EE14c895A23FC2D1d05278693d503BedF';
+export const contractAddress = '0xB07CaA395814FF99EbFb8cb77DF2466B639FCbf5';
 
 export const contractAddressERC20 = '0xa5Eeb3661ED2FA716E7DB8A9B8ec5d07274d4a71';
 
