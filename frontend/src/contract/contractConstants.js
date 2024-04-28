4906d640b9b989e61f964f0f517186a2cd7b0982
Zhuohua Zhong <zhong_zhuohua@outlook.com>
2024-04-28 17:35:45 +0800

    update name

--- frontend/src/contract/contractConstants.js
+++ frontend/src/contract/contractConstants.js
@@ -1,4 +1,4 @@
 export const contractAddress = '0xdc6d6725bee38A79cB32723adBD3E622219AdF96';
 
-export const contractERC20Address = '0xa5Eeb3661ED2FA716E7DB8A9B8ec5d07274d4a71';
+export const contractAddressERC20 = '0xa5Eeb3661ED2FA716E7DB8A9B8ec5d07274d4a71';
 
