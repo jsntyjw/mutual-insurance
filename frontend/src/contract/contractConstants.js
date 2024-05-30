52377db89fb8fbeda1872149b62c0ab72a55ea8a
Zhuohua Zhong <36853579+ZhuohuaZhong@users.noreply.github.com>
2024-05-30 21:50:12 +0800

    Update contract address on OP Sepolia

--- frontend/src/contract/contractConstants.js
+++ frontend/src/contract/contractConstants.js
@@ -1,4 +1,7 @@
-export const contractAddress = '0x1E8918ee0a4D415745714beC753EA1b883459Da0';
+// Ethereum Sepolia contract address with 100 SGDT faucet
+// export const contractAddress = '0x1E8918ee0a4D415745714beC753EA1b883459Da0';
+// export const contractAddressERC20 = '0xa5Eeb3661ED2FA716E7DB8A9B8ec5d07274d4a71';
 
-export const contractAddressERC20 = '0xa5Eeb3661ED2FA716E7DB8A9B8ec5d07274d4a71';
-
+// OP Sepolia contract address with 10000 SGDT faucet
+export const contractAddress = '0x874Ef5BA019aA021A0E26f95bd2e3EDB1D99ae56';
+export const contractAddressERC20 = '0xff77f043Aa81eFc1Ac6D008dfdA1D9bCC7a1dCc0';
